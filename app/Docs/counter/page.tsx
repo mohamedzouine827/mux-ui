68182a350ccbb30d7c2f703526b1822a0b59c2a3
mohamedzouine827 <mohamedzouine827@gmail.com>
2024-09-21 20:28:43 +0100

    Updated: code of the counter's page

--- app/Docs/counter/page.tsx
+++ app/Docs/counter/page.tsx
@@ -9,42 +9,41 @@ import Badge from '../badge/Badge';
 import Counter from './Counter';
 
 const Page: React.FC = () => {
-    const [showCodeStates, setShowCodeStates] = useState({
-        preview: false,
-        example1: false,
-        example2: false,
-        Random: false,
-    });
-    const [copied, setCopied] = useState(false);
+  const [showCodeStates, setShowCodeStates] = useState({
+    preview: false,
+    example1: false,
+    example2: false,
+    Random: false,
+  });
+  const [copied, setCopied] = useState(false);
 
-    const toggleCodeView = (section: keyof typeof showCodeStates) => {
-        setShowCodeStates(prev => ({
-            ...prev,
-            [section]: !prev[section]
-        }));
-    };
+  const toggleCodeView = (section: keyof typeof showCodeStates) => {
+    setShowCodeStates(prev => ({
+      ...prev,
+      [section]: !prev[section]
+    }));
+  };
 
-    const copyToClipboard = async () => {
-        try {
-            await navigator.clipboard.writeText(buttonCodePreview);
-            setCopied(true);
-            setTimeout(() => setCopied(false), 2000);
-        } catch (err) {
-            console.error('Failed to copy text: ', err);
-        }
-    };
+  const copyToClipboard = async () => {
+    try {
+      await navigator.clipboard.writeText(buttonCodePreview);
+      setCopied(true);
+      setTimeout(() => setCopied(false), 2000);
+    } catch (err) {
+      console.error('Failed to copy text: ', err);
+    }
+  };
 
-    const buttonCodePreview = `
+  const buttonCodePreview = `
 "use client";
 import React, { useEffect, useState } from 'react';
-import { motion, animate, AnimationPlaybackControls, useMotionValue, useTransform } from 'framer-motion';
+import { motion, animate, AnimationPlaybackControls, useMotionValue} from 'framer-motion';
 
 interface CounterProps {
   targetNumber: number;
   variant?: 'normal' | 'reverse' | 'random' | 'casino';
   speed?: 'slow' | 'normal' | 'fast';
   textSize?: 'text-xs' | 'text-sm' | 'text-base' | 'text-lg' | 'text-xl' | 'text-2xl' | 'text-3xl' | 'text-4xl'; // Tailwind text sizes
-  fontSize?: number; // Font size in pixels
   textColor?: string; // Text color (hex, rgb, or named colors)
   fontWeight?: 'font-thin' | 'font-extralight' | 'font-light' | 'font-normal' | 'font-medium' | 'font-semibold' | 'font-bold' | 'font-extrabold' | 'font-black'; // Tailwind font weights
 }
@@ -54,13 +53,11 @@ const Counter: React.FC<CounterProps> = ({
   variant = 'normal',
   speed = 'normal',
   textSize = 'text-6xl', // Default size
-  fontSize,
   textColor = 'text-blue-600', // Default color
   fontWeight = 'font-bold' // Default font weight
 }) => {
   const [digits, setDigits] = useState<number[]>(Array.from(String(targetNumber), Number)); 
-  const count = useMotionValue(variant === 'reverse' ? targetNumber : 0); 
-  const rounded = useTransform(count, (value) => Math.floor(value));
+  const count = useMotionValue(variant === 'reverse' ? targetNumber : 0);
 
   const generateRandomDigit = () => Math.floor(Math.random() * 10);
 
@@ -146,7 +143,6 @@ const Counter: React.FC<CounterProps> = ({
   }, [targetNumber, variant, speed, count]);
 
   const textStyle = {
-    fontSize: fontSize ? \`\${fontSize}px\` : undefined,
     color: textColor ? textColor : undefined,
   };
 
@@ -167,164 +163,165 @@ export default Counter;
 `.trim();
 
 
-    const buttonCodeExample1 = `
+
+  const buttonCodeExample1 = `
     <Counter targetNumber={256} textColor='#ffffff' variant="reverse"/>
     `.trim();
 
-    const buttonCodeExample2 = `
+  const buttonCodeExample2 = `
     <Counter targetNumber={256} textColor='#ffffff' variant="casino"/>
     `.trim();
 
-    const buttonCodeFunky = `
+  const buttonCodeFunky = `
     <Counter targetNumber={256} textColor='#ffffff' variant="random"/>
     `.trim();
 
 
 
 
-    const renderSection = (section: keyof typeof showCodeStates, title: string, description: string) => {
-        let buttonCode;
-        switch (section) {
-            case 'example1':
-                buttonCode = buttonCodeExample1;
-                break;
-            case 'example2':
-                buttonCode = buttonCodeExample2;
-                break;
-            case 'Random':
-                buttonCode = buttonCodeFunky;
-                break;
-            default:
-                buttonCode = buttonCodePreview;
-        }
-
-        return (
-            <section className="space-y-8">
-                <div className="space-y-4">
-                    <h2 className="text-2xl font-semibold text-white">{title}</h2>
-                    <p className="text-lg text-zinc-300">{description}</p>
-                </div>
-                <div className="space-y-6">
-                    <div className="flex flex-row justify-between border-b border-zinc-700">
-                        <div className='flex flex-row gap-8'>
-                            <button
-                                className={`text-base pb-2 ${!showCodeStates[section] ? 'text-zinc-300 border-b-2 border-zinc-300' : 'text-zinc-300'}`}
-                                onClick={() => toggleCodeView(section)}
-                            >
-                                Preview
-                            </button>
-                            <button
-                                className={`text-base pb-2 ${showCodeStates[section] ? 'text-zinc-300 border-b-2 border-zinc-300' : 'text-zinc-300'}`}
-                                onClick={() => toggleCodeView(section)}
-                            >
-                                Code
-                            </button>
-                        </div>
-                        {section === "preview" && (
-                            <div
-                                className='text-white flex flex-row gap-1 bg-[#666565] w-8 h-8 items-center justify-center rounded-[4px] cursor-pointer'
-                                onClick={copyToClipboard}
-                            >
-                                {copied ? (
-                                    <span>✓</span>
-                                ) : (
-                                    <Copy />
-                                )}
-                            </div>
-                        )}
-                    </div>
-
-                    <div className="p-6 max-h-[350px] bg-white bg-opacity-10 rounded-xl border border-zinc-700 backdrop-filter backdrop-blur-lg overflow-auto">
-                        {showCodeStates[section] ? (
-                            <SyntaxHighlighter
-                                language="jsx"
-                                style={vscDarkPlus}
-                                customStyle={{
-                                    background: 'transparent',
-                                    padding: '0',
-                                    margin: '0',
-                                }}
-                                codeTagProps={{
-                                    style: {
-                                        fontSize: '14px',
-                                        lineHeight: '1.5',
-                                    }
-                                }}
-                            >
-                                {buttonCode}
-                            </SyntaxHighlighter>
-                        ) : (
-                            <div className="flex flex-col items-center max-h-56 justify-center min-h-full space-y-4">
-                                <Counter targetNumber={256} textColor='#ffffff' variant={ section==="example1"? "reverse" : section==="example2" ? "casino" : section==="Random" ? "random" : undefined }/>
-                            </div>
-                        )}
-                    </div>
-                </div>
-
-            </section>
-        );
-    };
+  const renderSection = (section: keyof typeof showCodeStates, title: string, description: string) => {
+    let buttonCode;
+    switch (section) {
+      case 'example1':
+        buttonCode = buttonCodeExample1;
+        break;
+      case 'example2':
+        buttonCode = buttonCodeExample2;
+        break;
+      case 'Random':
+        buttonCode = buttonCodeFunky;
+        break;
+      default:
+        buttonCode = buttonCodePreview;
+    }
 
     return (
-        <Layout>
-            <div className="mx-auto px-14 py-12 space-y-8 max-w-4xl">
-                <header className="space-y-3">
-                    <h1 className="text-[32px] font-semibold text-white">Counter</h1>
-                    <p className="text-lg text-zinc-300 font-medium">Displays a Counter or a component that looks like a button.</p>
-                </header>
-
-                {renderSection('preview', 'Preview', "Here's a preview of Counter components with a code.")}
-                {renderSection('example1', 'Examples', "Example of the counter on reverse mode")}
-                {renderSection('example2', 'Animated Example', "An example of counter on casino mode")}
-                {renderSection('Random', 'Random Effect Example', "An example of counter on random mode")}
-                <div className="space-y-4">
-                    <h2 className="text-2xl font-semibold text-white">Props</h2>
-                    <table className="w-full table-auto border-collapse border border-gray-300">
-                        <thead className="">
-                            <tr>
-                                <th className="px-4 py-2 text-left text-base text-white font-semibold border border-gray-300">Prop</th>
-                                <th className="px-4 py-2 text-left text-zinc-300 font-semibold border border-gray-300">Type</th>
-                                <th className="px-4 py-2 text-left text-zinc-300 font-semibold border border-gray-300">Description</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <tr className="">
-                                <td className="px-4 py-2 text-sm font-medium text-zinc-300 border border-gray-300"><Badge>targetNumber</Badge></td>
-                                <td className="px-4 py-2 text-sm text-zinc-300 border border-gray-300">Number</td>
-                                <td className="px-4 py-2 text-sm text-zinc-300 border border-gray-300">The number that you want to do count effect on</td>
-                            </tr>
-                            <tr className="">
-                                <td className="px-4 py-2 text-sm font-medium text-zinc-300 border border-gray-300"><Badge>variant</Badge></td>
-                                <td className="px-4 py-2 text-sm text-zinc-300 border border-gray-300">String</td>
-                                <td className="px-4 py-2 text-sm text-zinc-300 border border-gray-300">The type of counter&apos;s animation</td>
-                            </tr>
-                            <tr className="">
-                                <td className="px-4 py-2 text-sm font-medium text-zinc-300 border border-gray-300"><Badge>Speed</Badge></td>
-                                <td className="px-4 py-2 text-sm text-zinc-300 border border-gray-300">String</td>
-                                <td className="px-4 py-2 text-sm text-zinc-300 border border-gray-300">The speed of counting (Slow, Normal and fast)</td>
-                            </tr>
-                            <tr className="">
-                                <td className="px-4 py-2 text-sm font-medium text-zinc-300 border border-gray-300"><Badge>textSize</Badge></td>
-                                <td className="px-4 py-2 text-sm text-zinc-300 border border-gray-300">String</td>
-                                <td className="px-4 py-2 text-sm text-zinc-300 border border-gray-300">Just tailwind text&apos;s classes</td>
-                            </tr>
-                            <tr className="">
-                                <td className="px-4 py-2 text-sm font-medium text-zinc-300 border border-gray-300"><Badge>textColor</Badge></td>
-                                <td className="px-4 py-2 text-sm text-zinc-300 border border-gray-300">String</td>
-                                <td className="px-4 py-2 text-sm text-zinc-300 border border-gray-300">The color of the numbers</td>
-                            </tr>
-                            <tr className="">
-                                <td className="px-4 py-2 text-sm font-medium text-zinc-300 border border-gray-300"><Badge>fontWeight</Badge></td>
-                                <td className="px-4 py-2 text-sm text-zinc-300 border border-gray-300">String</td>
-                                <td className="px-4 py-2 text-sm text-zinc-300 border border-gray-300">The font weight of the text</td>
-                            </tr>
-
-                        </tbody>
-                    </table>
-                </div>
+      <section className="space-y-8">
+        <div className="space-y-4">
+          <h2 className="text-2xl font-semibold text-white">{title}</h2>
+          <p className="text-lg text-zinc-300">{description}</p>
+        </div>
+        <div className="space-y-6">
+          <div className="flex flex-row justify-between border-b border-zinc-700">
+            <div className='flex flex-row gap-8'>
+              <button
+                className={`text-base pb-2 ${!showCodeStates[section] ? 'text-zinc-300 border-b-2 border-zinc-300' : 'text-zinc-300'}`}
+                onClick={() => toggleCodeView(section)}
+              >
+                Preview
+              </button>
+              <button
+                className={`text-base pb-2 ${showCodeStates[section] ? 'text-zinc-300 border-b-2 border-zinc-300' : 'text-zinc-300'}`}
+                onClick={() => toggleCodeView(section)}
+              >
+                Code
+              </button>
             </div>
-        </Layout>
+            {section === "preview" && (
+              <div
+                className='text-white flex flex-row gap-1 bg-[#666565] w-8 h-8 items-center justify-center rounded-[4px] cursor-pointer'
+                onClick={copyToClipboard}
+              >
+                {copied ? (
+                  <span>✓</span>
+                ) : (
+                  <Copy />
+                )}
+              </div>
+            )}
+          </div>
+
+          <div className="p-6 max-h-[350px] bg-white bg-opacity-10 rounded-xl border border-zinc-700 backdrop-filter backdrop-blur-lg overflow-auto">
+            {showCodeStates[section] ? (
+              <SyntaxHighlighter
+                language="jsx"
+                style={vscDarkPlus}
+                customStyle={{
+                  background: 'transparent',
+                  padding: '0',
+                  margin: '0',
+                }}
+                codeTagProps={{
+                  style: {
+                    fontSize: '14px',
+                    lineHeight: '1.5',
+                  }
+                }}
+              >
+                {buttonCode}
+              </SyntaxHighlighter>
+            ) : (
+              <div className="flex flex-col items-center max-h-56 justify-center min-h-full space-y-4">
+                <Counter targetNumber={256} textColor='#ffffff' variant={section === "example1" ? "reverse" : section === "example2" ? "casino" : section === "Random" ? "random" : undefined} />
+              </div>
+            )}
+          </div>
+        </div>
+
+      </section>
     );
+  };
+
+  return (
+    <Layout>
+      <div className="mx-auto px-14 py-12 space-y-8 max-w-4xl">
+        <header className="space-y-3">
+          <h1 className="text-[32px] font-semibold text-white">Counter</h1>
+          <p className="text-lg text-zinc-300 font-medium">Displays a Counter or a component that looks like a button.</p>
+        </header>
+
+        {renderSection('preview', 'Preview', "Here's a preview of Counter components with a code.")}
+        {renderSection('example1', 'Examples', "Example of the counter on reverse mode")}
+        {renderSection('example2', 'Animated Example', "An example of counter on casino mode")}
+        {renderSection('Random', 'Random Effect Example', "An example of counter on random mode")}
+        <div className="space-y-4">
+          <h2 className="text-2xl font-semibold text-white">Props</h2>
+          <table className="w-full table-auto border-collapse border border-gray-300">
+            <thead className="">
+              <tr>
+                <th className="px-4 py-2 text-left text-base text-white font-semibold border border-gray-300">Prop</th>
+                <th className="px-4 py-2 text-left text-zinc-300 font-semibold border border-gray-300">Type</th>
+                <th className="px-4 py-2 text-left text-zinc-300 font-semibold border border-gray-300">Description</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr className="">
+                <td className="px-4 py-2 text-sm font-medium text-zinc-300 border border-gray-300"><Badge>targetNumber</Badge></td>
+                <td className="px-4 py-2 text-sm text-zinc-300 border border-gray-300">Number</td>
+                <td className="px-4 py-2 text-sm text-zinc-300 border border-gray-300">The number that you want to do count effect on</td>
+              </tr>
+              <tr className="">
+                <td className="px-4 py-2 text-sm font-medium text-zinc-300 border border-gray-300"><Badge>variant</Badge></td>
+                <td className="px-4 py-2 text-sm text-zinc-300 border border-gray-300">String</td>
+                <td className="px-4 py-2 text-sm text-zinc-300 border border-gray-300">The type of counter&apos;s animation</td>
+              </tr>
+              <tr className="">
+                <td className="px-4 py-2 text-sm font-medium text-zinc-300 border border-gray-300"><Badge>Speed</Badge></td>
+                <td className="px-4 py-2 text-sm text-zinc-300 border border-gray-300">String</td>
+                <td className="px-4 py-2 text-sm text-zinc-300 border border-gray-300">The speed of counting (Slow, Normal and fast)</td>
+              </tr>
+              <tr className="">
+                <td className="px-4 py-2 text-sm font-medium text-zinc-300 border border-gray-300"><Badge>textSize</Badge></td>
+                <td className="px-4 py-2 text-sm text-zinc-300 border border-gray-300">String</td>
+                <td className="px-4 py-2 text-sm text-zinc-300 border border-gray-300">Just tailwind text&apos;s classes</td>
+              </tr>
+              <tr className="">
+                <td className="px-4 py-2 text-sm font-medium text-zinc-300 border border-gray-300"><Badge>textColor</Badge></td>
+                <td className="px-4 py-2 text-sm text-zinc-300 border border-gray-300">String</td>
+                <td className="px-4 py-2 text-sm text-zinc-300 border border-gray-300">The color of the numbers</td>
+              </tr>
+              <tr className="">
+                <td className="px-4 py-2 text-sm font-medium text-zinc-300 border border-gray-300"><Badge>fontWeight</Badge></td>
+                <td className="px-4 py-2 text-sm text-zinc-300 border border-gray-300">String</td>
+                <td className="px-4 py-2 text-sm text-zinc-300 border border-gray-300">The font weight of the text</td>
+              </tr>
+
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </Layout>
+  );
 };
 
 export default Page;
